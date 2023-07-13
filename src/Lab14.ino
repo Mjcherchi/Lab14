a5fc6225a998b5d2817b74c55d13b09e1cd65b79
Michael Cherchi <mjcherchi@gmail.com>
2023-07-13 13:37:28 -0500

    first commit

---
+++ src/Lab14.ino
@@ -0,0 +1,70 @@
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
+
+#include "oled-wing-adafruit.h"
+
+OledWingAdafruit display;
+
+int counter = 0;
+
+volatile bool INA = false;
+
+void setup()
+{
+    pinMode(D6, INPUT);
+    pinMode(D5, INPUT);
+
+    display.setup();
+
+    display.clearDisplay();
+
+    attachInterrupt(D6, COUNTERisr, FALLING);
+    attachInterrupt(D5, oNOFFisr, FALLING);
+}
+
+void loop()
+{
+    display.loop();
+    display.clearDisplay();
+    display.setTextSize(3);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 0);
+    display.println(counter);
+    display.display();
+
+    if (digitalRead(D5) && counter == 0)
+    {
+        interrupts();
+    }
+
+    if (INA = false)
+    {
+        display.loop();
+        display.clearDisplay();
+        display.setTextSize(3);
+        display.setTextColor(WHITE);
+        display.setCursor(0, 0);
+        display.println("Interrupts OFF");
+        display.display();
+    }
+}
+
+void COUNTERisr()
+{
+    counter++;
+    interrupts();
+}
+
+void oNOFFisr()
+{
+    INA = false;
+    counter = 0;
+    noInterrupts();
+    display.loop();
+    display.clearDisplay();
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 0);
+    display.println("Interrupts OFF");
+    display.display();
+}
